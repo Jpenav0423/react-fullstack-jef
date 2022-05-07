9caf54d36c5d82296892a508a97c35fc01e9e869
Jpenav0423 <jdpena0423@gmail.com>
2022-05-06 18:06:43 -0600

    Solucion Login
    Estaba mal escrito password

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -2,16 +2,15 @@ import React, {useState} from 'react';
 import axios from "axios";
 
 function Login() {
-
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
 
     const login = () => {
-        const data = {username: username, paswword: password};
+        const data = { username: username, password: password};
         axios.post("http://localhost:3001/auth/login",data).then((response) => {
             console.log(response.data);
-        })
-    }
+        });
+    };
     
   return (
     <div>
