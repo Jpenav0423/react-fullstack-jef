b75c0b5418b213ef484f23efcb76197ea409d71d
Jpenav0423 <jdpena0423@gmail.com>
2022-05-10 14:16:58 -0600

    AuthCode
    Se genero un codigo de autenticacion

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,36 +1,45 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import axios from "axios";
+import {useNavigate} from "react-router-dom";
+
 
 function Login() {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-    const login = () => {
-        const data = { username: username, password: password};
-        axios.post("http://localhost:3001/auth/login",data).then((response) => {
-            console.log(response.data);
-        });
-    };
-    
+  let navigate = useNavigate();
+
+  const login = () => {
+    const data = { username: username, password: password };
+    axios.post("http://localhost:3001/auth/login", data).then((response) => {
+      if(response.data.error) {
+        alert(response.data.error);
+    } else {
+        sessionStorage.setItem("accessToken", response.data)
+        navigate("/");
+      }
+    });
+  };
   return (
-    <div>
-        <input 
-        type="text" 
+    <div className="loginContainer">
+      <label>Username:</label>
+      <input
+        type="text"
         onChange={(event) => {
-        setUsername(event.target.value);
+          setUsername(event.target.value);
         }}
-        />
-
-        <input 
+      />
+      <label>Password:</label>
+      <input
         type="password"
-        onChange={(event => {
-        setPassword(event.target.value);
-        })}
-        />
+        onChange={(event) => {
+          setPassword(event.target.value);
+        }}
+      />
 
-        <button onClick={login}>Login</button>
+      <button onClick={login}> Login </button>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
